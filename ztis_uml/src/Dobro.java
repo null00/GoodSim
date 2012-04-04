8b6a1febcb61784ffaf0b048f96ea9b68b9e5c8d
Mateusz Rudnicki <rudnicki@student.agh.edu.pl>
2012-04-04 12:38:57 +0200

    setters and getters for DokumentIntencji

--- ztis_uml/src/Dobro.java
+++ ztis_uml/src/Dobro.java
@@ -1,4 +1,3 @@
-
 public class Dobro {
 
 }
--- ztis_uml/src/DokumentIntencji.java
+++ ztis_uml/src/DokumentIntencji.java
@@ -4,4 +4,41 @@ public class DokumentIntencji {
 	private int idKonsumenta;
 	private List<Dobro> dobroIn;
 	private List<Dobro> dobroOut;
+
+	public DokumentIntencji() {
+		super();
+	}
+
+	public DokumentIntencji(int idKonsumenta, List<Dobro> dobroIn,
+			List<Dobro> dobroOut) {
+		super();
+		this.idKonsumenta = idKonsumenta;
+		this.dobroIn = dobroIn;
+		this.dobroOut = dobroOut;
+	}
+
+	public int getIdKonsumenta() {
+		return idKonsumenta;
+	}
+
+	public void setIdKonsumenta(int idKonsumenta) {
+		this.idKonsumenta = idKonsumenta;
+	}
+
+	public List<Dobro> getDobroIn() {
+		return dobroIn;
+	}
+
+	public void setDobroIn(List<Dobro> dobroIn) {
+		this.dobroIn = dobroIn;
+	}
+
+	public List<Dobro> getDobroOut() {
+		return dobroOut;
+	}
+
+	public void setDobroOut(List<Dobro> dobroOut) {
+		this.dobroOut = dobroOut;
+	}
+
 }
