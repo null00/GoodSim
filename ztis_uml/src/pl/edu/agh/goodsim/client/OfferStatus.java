2f00e7595b3cceee28341e8e80f236ff0914cd7c
Mateusz Rudnicki <rudnicki@student.agh.edu.pl>
2012-05-02 14:50:20 +0200

    OfferStatus and TaskValues getters/setters added

--- ztis_uml/src/pl/edu/agh/goodsim/client/OfferStatus.java
+++ ztis_uml/src/pl/edu/agh/goodsim/client/OfferStatus.java
@@ -15,4 +15,50 @@ public class OfferStatus {
 	private Date receivedDate;
 	private Offer offer;
 	private Boolean refusal;
+
+	public OfferStatus(String contractorid, ClientOffer intention) {
+		super();
+		this.contractorid = contractorid;
+		this.intention = intention;
+	}
+
+	public Date getSendedIntentionDate() {
+		return sendedIntentionDate;
+	}
+
+	public void setSendedIntentionDate(Date sendedIntentionDate) {
+		this.sendedIntentionDate = sendedIntentionDate;
+	}
+
+	public Date getReceivedDate() {
+		return receivedDate;
+	}
+
+	public void setReceivedDate(Date receivedDate) {
+		this.receivedDate = receivedDate;
+	}
+
+	public Offer getOffer() {
+		return offer;
+	}
+
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
+
+	public Boolean getRefusal() {
+		return refusal;
+	}
+
+	public void setRefusal(Boolean refusal) {
+		this.refusal = refusal;
+	}
+
+	public String getContractorid() {
+		return contractorid;
+	}
+
+	public ClientOffer getIntention() {
+		return intention;
+	}
 }
--- ztis_uml/src/pl/edu/agh/goodsim/client/TaskValues.java
+++ ztis_uml/src/pl/edu/agh/goodsim/client/TaskValues.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.goodsim.client;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,4 +16,41 @@ public class TaskValues {
 	private List<List<Map<String, OfferStatus>>> offers;
 	private List<Good> goodsForSale;
 	private List<Good> goodsReceived;
+
+	public TaskValues(Contract contract) {
+		this.contract = contract;
+		// offers = new LinkedList<List<Map<String, OfferStatus>>>();
+		initOffers();
+		goodsForSale = new LinkedList<Good>();
+		goodsReceived = new LinkedList<Good>();
+	}
+
+	public void initOffers() {
+		List<Map<String, OfferStatus>> negotiation = new LinkedList<Map<String, OfferStatus>>();
+		Map<String, OfferStatus> offerStatusMap = new HashMap<String, OfferStatus>();
+
+		offers = new LinkedList<List<Map<String, OfferStatus>>>();
+		offers.add(negotiation);
+		negotiation.add(offerStatusMap);
+	}
+
+	public Contract getContract() {
+		return contract;
+	}
+
+	public void setContract(Contract contract) {
+		this.contract = contract;
+	}
+
+	public List<List<Map<String, OfferStatus>>> getOffers() {
+		return offers;
+	}
+
+	public List<Good> getGoodsForSale() {
+		return goodsForSale;
+	}
+
+	public List<Good> getGoodsReceived() {
+		return goodsReceived;
+	}
 }
