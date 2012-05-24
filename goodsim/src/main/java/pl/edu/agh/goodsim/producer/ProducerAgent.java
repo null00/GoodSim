d623f6dbcf43a0408cc4cb7e18ad4daca4ea715a
krzyho <krzyho@gmail.com>
2012-05-24 22:02:42 +0200

    new methods (without implementation) for ProducerAgent

--- goodsim/src/main/java/pl/edu/agh/goodsim/producer/ProducerAgent.java
+++ goodsim/src/main/java/pl/edu/agh/goodsim/producer/ProducerAgent.java
@@ -1,11 +1,27 @@
 package pl.edu.agh.goodsim.producer;
 
 import pl.edu.agh.goodsim.document.ClientOffer;
+import pl.edu.agh.goodsim.entity.Good;
+
+import java.util.List;
 
 public class ProducerAgent {
-    
+    private List<Good> storedGoods;
+
     public void receiveIntention(ClientOffer offer) {
         // TODO
+    }
+
+    public void receiveCounteroffer(ClientOffer counterOffer) {
+        //TODO
+    }
+
+    public void receiveSignOffer(ClientOffer signOffer) {
+        //TODO
+    }
+
+    public void storeGood(Good good) {
+        storedGoods.add(good);
     }
 
 }
