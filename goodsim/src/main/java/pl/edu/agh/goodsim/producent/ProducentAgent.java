21c912a332bda9defed5ad821fd139877ce8665f
Jakub Krawetkowski <krawetko@gmail.com>
2012-05-24 21:48:36 +0200

    Rename + ReceiveIntention

--- goodsim/src/main/java/pl/edu/agh/goodsim/producent/ProducentAgent.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.goodsim.producent;
-
-public class ProducentAgent {
-
-}
---
+++ goodsim/src/main/java/pl/edu/agh/goodsim/producent/ProducerAgent.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.goodsim.producent;
+
+import pl.edu.agh.goodsim.document.ClientOffer;
+
+public class ProducerAgent {
+    
+    public void receiveIntention(ClientOffer offer) {
+        // TODO
+    }
+
+}
