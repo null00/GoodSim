f0e93794ab253ffff603474c8af4726c6c237e0f
Jaroslaw Szczesniak <null@student.agh.edu.pl>
2012-06-14 21:23:40 +0200

    Updating linux scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 cd `dirname $0`
@@ -6,5 +5,7 @@ BASEDIR=`pwd`
 cd -
 
 cd $BASEDIR/goodsim
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=${project}./../sources/jademx.jar -DgroupId=jade.jademx -DartifactId=jademx -Dversion=0.4.1 -Dpackaging=jar -DlocalRepositoryPath=$BASEDIR/goodsim/local-repo
+mvn org.apache.maven.plugins:maven-install-plugin:2.3.1:install-file -Dfile=${project}./../sources/jade.jar -DgroupId=com.tilab.jade -DartifactId=jade -Dversion=3.4.0 -Dpackaging=jar -DlocalRepositoryPath=$BASEDIR/goodsim/local-repo
 mvn clean install assembly:single
 cd $BASEDIR
