2016cd596e30aae794d308134541c66533cf1ecc
Krzysiek Mycek <krzyho@gmail.com>
2012-06-14 21:22:38 +0200

    runAgent - enabled JMX connectivity

--- runAgent.bat
+++ runAgent.bat
@@ -5,4 +5,4 @@ set basedir=%~dp0
 set cp=%basedir%goodsim\target\goodsim-0.1-jar-with-dependencies.jar
 set client=pl.edu.agh.goodsim.client.ClientAgent
 set producer=pl.edu.agh.goodsim.producer.ProducerAgent
-java -cp %cp% pl.edu.agh.goodsim.jmx.JademxAgentTesting -Dcom.sun.management.jmxremote.port=1098 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false 
+java -cp %cp% -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false pl.edu.agh.goodsim.jmx.JademxAgentTesting
