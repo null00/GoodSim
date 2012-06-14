28ac18bbde4627c195e79a2ac412519ad1c65748
Mateusz Rudnicki <rudnicki@student.agh.edu.pl>
2012-06-14 21:44:16 +0200

    update linux scripts

--- runAgent.sh
+++ runAgent.sh
@@ -7,6 +7,7 @@ cd -
 jarfile=$BASEDIR/goodsim/target/goodsim-0.1-jar-with-dependencies.jar
 client=pl.edu.agh.goodsim.client.ClientAgent
 producer=pl.edu.agh.goodsim.producer.ProducerAgent
+
 java -cp $jarfile \
  -Dcom.sun.management.jmxremote.port=1099 \
  -Dcom.sun.management.jmxremote.authenticate=false \
